6234f2e9e591cdfff8f858823e342baf44b5f59f
Kirill Repkin <kirill@repkin.su>
2024-04-12 13:42:20 +0300

    initial commit

---
+++ app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+case $1 in
+    init)
+        echo executing \"$1\"
+        for fname in one two three four five; do cp data/files/big.txt data/files/big_${fname}.sample; done
+        echo done
+    ;;
+
+    devenv)
+        echo executing \"$1\"
+        docker compose -f docker-compose.yml up
+        echo done
+        ;;
+
+    *)
+        echo "unknown action \"$1\". nothing to do"
+        ;;
+esac
---
+++ src/main/java/ru/itone/beeline/App.java
@@ -0,0 +1,13 @@
+package ru.itone.beeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
