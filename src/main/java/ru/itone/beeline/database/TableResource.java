acbda30f6b60770a52dc124c05b6d8a0321d081f
Kirill Repkin <krepkin@it-one.ru>
2024-04-16 15:58:26 +0300

    jetty base app

--- src/main/java/ru/itone/beeline/database/TableResource.java
+++ src/main/java/ru/itone/beeline/database/TableResource.java
@@ -1,11 +1,14 @@
 package ru.itone.beeline.database;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/table")
 public class TableResource {
 
+    @GET
+    @Path("/list")
     public Response list() {
         return Response.ok().entity("OK").build();
     }
--- src/main/java/ru/itone/beeline/database/WebApp.java
+++ src/main/java/ru/itone/beeline/database/WebApp.java
@@ -1,18 +1,48 @@
 package ru.itone.beeline.database;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import java.net.URI;
+import java.net.URL;
+import java.security.ProtectionDomain;
 
+import javax.ws.rs.core.UriBuilder;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ApplicationPath("/api")
-public class WebApp extends Application {
+public class WebApp {
 
     private static Logger log = LoggerFactory.getLogger(WebApp.class);
 
-    public static void main(String[] args) {
-        log.info("Application starting...");
+    public static void main(String[] args) throws Exception {
+        URI baseUri = UriBuilder.fromUri("http://localhost").port(8880).build();
+		ResourceConfig config = new ResourceConfig(TableResource.class);
+		Server server = JettyHttpContainerFactory.createServer(baseUri, config,false);
+
+		ContextHandler contextHandler = new ContextHandler("/rest");
+		contextHandler.setHandler(server.getHandler());
+		
+		ProtectionDomain protectionDomain = WebApp.class.getProtectionDomain();
+		URL location = protectionDomain.getCodeSource().getLocation();
+		
+		ResourceHandler resourceHandler = new ResourceHandler();
+		resourceHandler.setWelcomeFiles(new String[] {"index.html"});
+		resourceHandler.setResourceBase(location.toExternalForm());
+		System.out.println(location.toExternalForm());
+		HandlerCollection handlerCollection = new HandlerCollection();
+		handlerCollection.setHandlers(new Handler[] {
+            resourceHandler, contextHandler, new DefaultHandler()});
+		server.setHandler(handlerCollection);
+		
+		server.start();
+		server.join();
     }
 
 }
