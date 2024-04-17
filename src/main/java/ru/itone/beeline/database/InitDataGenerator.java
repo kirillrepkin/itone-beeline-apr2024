0044e6831ab08d60783a86c7e9c6550cfa3b77c0
Kirill Repkin <kirill@repkin.su>
2024-04-17 13:48:29 +0300

    refactoring

--- src/main/java/ru/itone/beeline/database/InitDataGenerator.java
+++ src/main/java/ru/itone/beeline/database/InitDataGenerator.java
@@ -11,42 +11,50 @@ import ru.itone.beeline.database.entity.UserWebVisit;
 
 public class InitDataGenerator {
 
+    private static final String DEFAULT_FILE_PATH = "data/sql/init.sql";
+
+    private static final int DEFAULT_ROWS_COUNT = 100;
+
+    private static final String SQL_TABLE_DDL = """
+    create table if not exists `public`.`user_web_visit` (
+        `login` varchar(255),
+        `location` varchar(255),
+        `date_time_ts` timestamp default current_timestamp
+    );
+    """;
+    
+    private static final String SQL_INSERT_PREFIX = """
+    insert into `public`.`user_web_visit` (`login`, `location`, `date_time_ts`) values
+    """;
+    
+    private static final String NEW_LINE = "\r\n";
+
     private static Logger log = LoggerFactory.getLogger(InitDataGenerator.class);
 
     public static void main(String[] args) {
+        long count = args.length >= 1 ? Long.parseLong(args[0]) : DEFAULT_ROWS_COUNT;
+        String output = args.length >= 2 ? args[1] : DEFAULT_FILE_PATH;
+        log.info("Generating {} with {} rows", output, count);
+        generateSqlFile(count, output);
+    }
 
-        long count = args.length >= 1 ? Long.parseLong(args[0]) : 100;
-        String output = args.length >= 2 ? args[1] : "data/sql/init.sql";
-
-        final String tableDDL = """
-        create table if not exists `public`.`user_web_visit` (
-            `login` varchar(255),
-            `location` varchar(255),
-            `date_time_ts` timestamp default current_timestamp
-        );
-        """;
-        
-        final String insertPrefix = """
-        insert into `public`.`user_web_visit` (`login`, `location`, `date_time_ts`) values
-        """;
-        
-        final String nl = "\r\n";
-        
+    private static void generateSqlFile(long count, String output) {
         File file = new File(output);
         if(file.exists()) {
             file.delete();
         }
 
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-            bw.write(tableDDL);
-            bw.write(nl);
-            bw.write(insertPrefix);
+            bw.write(SQL_TABLE_DDL);
+            bw.write(NEW_LINE);
+            bw.write(SQL_INSERT_PREFIX);
             String line;
             for(int i=0; i<count; i++) {
                 UserWebVisit row = UserWebVisit.random();
-                line = "\t('"+row.getLogin()+"', '"+row.getLocation()+"', '"+row.getDateTime().minusMinutes(Long.valueOf(count/60).intValue())+"')";
+                line = "\t('"+row.getLogin()+"', '"+row.getLocation()+"', '"
+                    +row.getDateTime().minusMinutes(Long.valueOf(count/60).intValue())+"')";
                 bw.write(line);
-                bw.write(i==count-1 ? ";" : ","+nl);
+                bw.write(i==count-1 ? ";" : ","+NEW_LINE);
             }
         } catch(Exception ex) {
             log.error(ex.getMessage(), ex);
--- src/main/java/ru/itone/beeline/database/WebApp.java
+++ src/main/java/ru/itone/beeline/database/WebApp.java
@@ -16,14 +16,18 @@ import ru.itone.beeline.database.resource.TableResource;
 
 public class WebApp {
 
-    private static Logger log = LoggerFactory.getLogger(WebApp.class);
+    private static final String BASE_PATH = "/api";
+	private static final int PORT = 8880;
+	private static final String HOSTNAME = "http://0.0.0.0";
 
+	private static Logger log = LoggerFactory.getLogger(WebApp.class);
+	
     public static void main(String[] args) throws Exception {
-        URI baseUri = UriBuilder.fromUri("http://0.0.0.0").port(8880).build();
+        URI baseUri = UriBuilder.fromUri(HOSTNAME).port(PORT).build();
 		ResourceConfig config = new ResourceConfig(TableResource.class);
 		Server server = JettyHttpContainerFactory.createServer(baseUri, config,false);
 
-		ContextHandler contextHandler = new ContextHandler("/api");
+		ContextHandler contextHandler = new ContextHandler(BASE_PATH);
 		contextHandler.setHandler(server.getHandler());
 				
 		HandlerCollection handlerCollection = new HandlerCollection();
